1068ebf06707aaa4e261a9417ba543b49c8a4a01
Nushrate Ahmed <75279929+nushratea@users.noreply.github.com>
2024-10-05 00:25:24 -0400

    Update server.R

--- Data Visualization Examples/R Language/R application/server.R
+++ Data Visualization Examples/R Language/R application/server.R
@@ -3,7 +3,7 @@ library(shiny)
 library(tidyverse)
 
 # Read in data
-adult <- read_csv("/Users/parisha/ShinyApp/Final Project/adult.csv")
+adult <- read_csv("adult.csv")
 # Convert column names to lowercase for convenience 
 names(adult) <- tolower(names(adult))
 
